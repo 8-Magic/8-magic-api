e571445eea055305e431f9b07daba7d797312d6d
Ali Almasi <a710almasi@gmail.com>
2025-07-21 15:21:26 +0330

    update: updated cronjob to use sb

--- src/app/cron/route.ts
+++ src/app/cron/route.ts
@@ -1,36 +1,79 @@
 import { NextResponse } from "next/server";
+import { Database } from "@/data/database.types";
+
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
 export async function GET(): Promise<NextResponse> {
-	const answer = await (
-		await fetch("https://api.8.alialmasi.ir/v1/answers")
-	).json();
+	const supabaseUrl = "https://vafyxywqmdqxzlzlrwvx.supabase.co";
+	const supabaseKey = process.env.SB_API_TOKEN as string;
+	const supabase: SupabaseClient = createClient<Database>(
+		supabaseUrl,
+		supabaseKey
+	);
 	const token = process.env.TG_BOT;
 	const chatId = process.env.TG_CHAT_ID;
-	const text = JSON.stringify({
-		timestamp: new Date().toISOString(),
-		status: answer.status
-	});
+	try {
+		const status = (await supabase.from("answers").select("*")).status;
 
-	const res = await fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json"
-		},
-		body: JSON.stringify({
-			chat_id: chatId,
-			text
-		})
-	});
+		if ((await supabase.from("answers").select("*")).error)
+			throw new Error("Supabase client faced an error");
 
-	const data = await res.json();
-
-	if (!data.ok) {
-		console.error("Telegram Error:", data);
-		return NextResponse.json(
-			{ success: false, error: data.description },
-			{ status: 500 }
+		const res = await fetch(
+			`https://api.telegram.org/bot${token}/sendMessage`,
+			{
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify({
+					chat_id: chatId,
+					text: {
+						timestamp: new Date().toISOString(),
+						status
+					}
+				})
+			}
 		);
-	}
 
-	return NextResponse.json({ success: true });
+		const data = await res.json();
+
+		if (!data.ok) {
+			console.error("Telegram Error:", data);
+			return NextResponse.json(
+				{ success: false, error: data.description },
+				{ status: 500 }
+			);
+		}
+
+		return NextResponse.json({ success: true });
+	} catch (error: unknown) {
+		const res = await fetch(
+			`https://api.telegram.org/bot${token}/sendMessage`,
+			{
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify({
+					chat_id: chatId,
+					text: {
+						timestamp: new Date().toISOString(),
+						error
+					}
+				})
+			}
+		);
+
+		const data = await res.json();
+
+		if (!data.ok) {
+			console.error("Telegram Error:", data);
+			return NextResponse.json(
+				{ success: false, error: data.description },
+				{ status: 500 }
+			);
+		}
+
+		return NextResponse.json({ success: false, error }, { status: 500 });
+	}
 }
