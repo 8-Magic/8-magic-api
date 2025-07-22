ec6870e2b82354ad67cc4634398dd043a8a7262a
Ali Almasi <a710almasi@gmail.com>
2025-07-22 03:51:39 +0330

    add: added 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Page() {
+	return (
+		<div className="flex flex-col items-center justify-center text-center select-none">
+			<h1 className="text-6xl text-red-400">404</h1>
+			<p className="pb-2 text-4xl text-red-100">Not found</p>
+		</div>
+	);
+}
