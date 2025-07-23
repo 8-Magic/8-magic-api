46259ce27f6248338dc5c42e347496b534f7b889
Ali Almasi <a710almasi@gmail.com>
2025-07-23 18:12:23 +0330

    update: updated supabaseclient to use public keys

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -4,11 +4,9 @@ import { createClient, SupabaseClient } from "@supabase/supabase-js";
 import { Err } from "@/data/types";
 import { randomElement } from "./randomAnswer";
 
-const supabaseUrl = "https://vafyxywqmdqxzlzlrwvx.supabase.co";
-const supabaseKey = process.env.SB_API_TOKEN as string;
 const supabase: SupabaseClient = createClient<Database>(
-	supabaseUrl,
-	supabaseKey
+	process.env.NEXT_PUBLIC_SUPABASE_URL!,
+	process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
 
 export default supabase;
@@ -49,4 +47,93 @@ export async function RandomAnswer(): Promise<DBanswerType> {
 		});
 
 	return updated?.[0] as DBanswerType;
+}
+
+export async function getAnswerbyID(id: number) {
+	if (id && typeof id === "number") {
+		const { data, error: fetchError } = await supabase
+			.from("answers")
+			.select("*")
+			.eq("id", id);
+
+		const fetchArray = data as DBanswerType[];
+
+		const answer: DBanswerType = randomElement(
+			fetchArray as Array<unknown>
+		) as DBanswerType;
+
+		if (fetchError)
+			throw new Err({
+				message: "Error while fetching data from database",
+				type: "UNKNOWN_ERR",
+				details: fetchError.message
+			});
+
+		const { data: updated, error: rpcError } = await supabase.rpc(
+			"increment_answer_count",
+			{ row_id: answer.id }
+		);
+
+		if (rpcError)
+			throw new Err({
+				message: "Error while running rpc database function",
+				type: "UNKNOWN_ERR",
+				details: rpcError.message
+			});
+
+		return updated?.[0] as DBanswerType;
+	} else
+		throw new Err({
+			message: "Requested ID is invalid",
+			type: "REQ_ERR",
+			code: 400,
+			cause: "getAnswerbyID() on /src/utils/supabaseClient.ts"
+		});
+}
+
+export async function getAllAnswers(
+	type: answerType | string
+): Promise<DBanswerType[]> {
+	if (type && typeof type === "string" && type !== "all") {
+		type = type.toLowerCase().trim();
+
+		const { data, error: fetchError } = await supabase
+			.from("answers")
+			.select("*")
+			.eq("type", type);
+
+		const fetchArray = data as DBanswerType[];
+
+		if (fetchError)
+			throw new Err({
+				message: "Error while fetching data from database",
+				type: "UNKNOWN_ERR",
+				details: fetchError.message
+			});
+
+		return fetchArray;
+	} else {
+		if (type === "all") {
+			const { data, error: fetchError } = await supabase
+				.from("answers")
+				.select("*");
+
+			const fetchArray = data as DBanswerType[];
+
+			if (fetchError)
+				throw new Err({
+					message: "Error while fetching data from database",
+					type: "UNKNOWN_ERR",
+					details: fetchError.message
+				});
+
+			return fetchArray;
+		} else
+			throw new Err({
+				message: "Requested ID is invalid",
+				type: "REQ_ERR",
+				code: 400,
+				cause: "getAnswerbyID() on /src/utils/supabaseClient.ts"
+			});
+	}
 }
