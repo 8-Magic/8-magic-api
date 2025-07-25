aab7bf6aebbd2663a72650cc06376afabc8dd8b2
Ali Almasi <a710almasi@gmail.com>
2025-07-26 03:09:12 +0330

    update(docs): v2 page

--- src/app/v2/page.tsx
+++ src/app/v2/page.tsx
@@ -1,3 +1,4 @@
+import { getAllAnswersLength } from "@/utils/supabaseClient";
 import { Metadata } from "next";
 import Link from "next/link";
 
@@ -5,32 +6,127 @@ export const metadata: Metadata = {
 	title: "v2"
 };
 
-export default function Page() {
+export default async function Page() {
 	return (
 		<>
 			<h1 className="header-1">API version 2</h1>
 			<p>
-				This version is under development. I&apos;m planning to use{" "}
+				This version is not exprimental or in development, but it&apos;s not as
+				fully stable as v1. We recommend you to use v1 as it&apos;s more stable,
+				but you can still use it. We&apos;re using{" "}
 				<Link href="https://supabase.com" className="link">
 					Supabase
 				</Link>{" "}
-				as database.
+				as database provider.
 			</p>
 			<p>
-				This can help me to see more info (like how many times my API is being
-				used).
+				This can help us to see more info (like how many times our API is being
+				used) and to have many more answer entries (which right now is{" "}
+				<Link href="/v2/answers/all" className="link">
+					{(await getAllAnswersLength()).all} answers
+				</Link>
+				.)
 			</p>
 			<p>
-				I&apos;ll create a public audit log to clearly show what data I collect
-				and how it&apos;s used.
+				Not storing all this amount answer objects in database (but, like in v1,
+				storing in static files &mdash;{" "}
+				<span className="text-sm">
+					see{" "}
+					<Link
+						href="https://github.com/8-Magic/8-magic-api/blob/main/src/data/answers.ts"
+						className="link"
+					>
+						/src/data/answers.ts
+					</Link>
+				</span>
+				) could make the perfomance really bad.
+			</p>
+			<h2 className="header-2">v2 changes</h2>
+			<p>
+				Pretty similar to v1, we have the same main endpoint{" "}
+				<code className="mono code-line">/answers</code> but with some changes.
 			</p>
 			<p>
-				For now, you can use{" "}
-				<Link className="link" href="/v2/answers">
-					/v2/answers
+				First, we don&apos;t have the{" "}
+				<code className="mono code-line">/answers/ID</code> sub-route, so you
+				can&apos;t select a specific answer object anymore.
+			</p>
+			<p>
+				Second, the &quot;?number&quot; parameters from the{" "}
+				<code className="mono code-line">/answers/all</code> sub-route has been
+				removed.
+			</p>
+			<h2 className="header-2">v2 answerObject type</h2>
+			<p>In v2, we have a different object type for answers.</p>
+			<pre className="code-block" aria-details="before-false">
+				{`type DBanswerType = {
+	id: number;
+	answer: string;
+	type: answerType;
+	count: number;
+};`}
+			</pre>
+			<p>
+				See{" "}
+				<Link
+					href="https://github.com/8-Magic/8-magic-api/blob/main/src/utils/supabaseClient.ts"
+					className="link"
+				>
+					/src/utils/supabaseClient.ts:14
 				</Link>{" "}
-				to test it.
+				for more info.
 			</p>
+			<h2 className="header-2">v2 endpoints</h2>
+			<p>With all that, let&apos;s go for the examples.</p>
+			<p className="endpoint">
+				<span className="grayit">(GET)</span> /answers
+			</p>
+			<pre className="code-block">
+				{`{
+  "status": "success",
+  "data": {
+    "answer": {
+      "id": 98,
+      "answer": "Turn back Now",
+      "type": "negative",
+      "count": 1
+    }
+  }
+}`}
+			</pre>
+			<p>
+				As you can see <span className="code-line">(&quot;id&quot;: 98)</span>,
+				we have way more answer objects available in v2.
+			</p>
+			<p className="endpoint">
+				<span className="grayit">(GET)</span> /answers/all
+			</p>
+			<pre className="code-block">
+				{`{
+  "status": "success",
+  "data": {
+    "type": "all",
+    "length": 132,
+    "answers": [
+      {
+        "id": 1,
+        "answer": "Without a doubt",
+        "type": "positive",
+        "count": 6,
+        "updated_at": "2025-07-23T19:44:12.286792+00:00"
+      },
+      {...},
+	  ...
+	]
+  }
+}`}
+			</pre>
+			<p>You&apos;ll receive all answers in an array</p>
+			<p>You can also specify &quot;?type | ?t&quot; on this subroute.</p>
+			<p className="endpoint">
+				<span className="grayit">(GET)</span> /answers/all?t=positive
+			</p>
+			<pre className="code-block">Why don&apos;t you try it yourself?</pre>
 		</>
 	);
 }
