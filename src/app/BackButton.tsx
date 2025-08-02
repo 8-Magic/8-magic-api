d45da661a00735bb365cf1543d1ea6f4cbdf5277
Ali Almasi <a710almasi@gmail.com>
2025-08-02 23:31:35 +0330

    change: backbutton logic and style

--- src/app/BackButton.tsx
+++ src/app/BackButton.tsx
@@ -1,26 +1,26 @@
 "use client";
 import JSONstring from "@/utils/JSON";
 import { randomAnswer } from "@/utils/randomAnswer";
-import { usePathname } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 
 export default function BackButton(): React.ReactNode {
 	const answer = randomAnswer();
+
+	const router = useRouter();
 	const path = usePathname();
+	const handleBackClick = () => {
+		const segments = path.split("/").filter(Boolean).slice(0, -1);
+		const newPath = "/" + segments.join("/");
+		router.push(newPath || "/");
+	};
+
 	if (path.includes("/audit_log")) return;
 	return path !== "/" ? (
 		<button
-			onClick={() =>
-				(window.location.href =
-					"/" +
-					window.location.pathname
-						.split("/")
-						.filter(Boolean)
-						.slice(0, -1)
-						.join("/"))
-			}
-			className="before:content-['^'] before:absolute before:text-stone-200 relative before:left-3 pl-8 px-2 py-2 hover:text-stone-900 hover:before:text-stone-900 hover:bg-stone-200 text-left cursor-pointer border w-fit"
+			onClick={handleBackClick}
+			className="before:content-['<-'] before:absolute before:text-stone-200 relative before:left-3 pl-10 px-4 py-2 hover:text-stone-900 hover:before:text-stone-900 hover:bg-stone-200 text-left cursor-pointer border w-fit"
 		>
-			Go Up
+			Go Back
 		</button>
 	) : (
 		<p className="text-stone-400" suppressHydrationWarning={true}>
